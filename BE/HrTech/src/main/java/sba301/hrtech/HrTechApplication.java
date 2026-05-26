47de5a73ec4fd5e982cd57ce1ca8b5051be928ea
myapah <myapah7605@gmail.com>
2026-05-26 19:33:06 +0700

    Set up BE environment

---
+++ BE/HrTech/src/main/java/sba301/hrtech/HrTechApplication.java
@@ -0,0 +1,13 @@
+package sba301.hrtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrTechApplication.class, args);
+    }
+
+}
