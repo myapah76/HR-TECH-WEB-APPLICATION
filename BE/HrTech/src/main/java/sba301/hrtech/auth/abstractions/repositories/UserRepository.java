ad4ce712c4ab7cccbf816d7404361b563afe19b6
myapah76 <140422521+myapah76@users.noreply.github.com>
2026-06-01 11:06:25 +0700

    chore

--- BE/HrTech/src/main/java/sba301/hrtech/auth/abstractions/repositories/UserRepository.java
+++ BE/HrTech/src/main/java/sba301/hrtech/auth/abstractions/repositories/UserRepository.java
@@ -1,12 +1,13 @@
 package sba301.hrtech.auth.abstractions.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import sba301.hrtech.auth.entities.User;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     List<User> findByEmailContains(String email);
 
