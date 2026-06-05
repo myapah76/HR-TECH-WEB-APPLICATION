6e1b4fa7570880c6f7f330b67e7f0dce37a3007b
duykhoiSD <duykhoi200805@gmail.com>
2026-06-05 15:43:48 +0700

    update upload pdf to cloudinary

--- BE/HrTech/src/main/java/sba301/hrtech/cv/services/CvService.java
+++ BE/HrTech/src/main/java/sba301/hrtech/cv/services/CvService.java
@@ -1,20 +1,17 @@
 package sba301.hrtech.cv.services;
 
+import org.springframework.web.multipart.MultipartFile;
 import sba301.hrtech.cv.entities.Cv;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface CvService {
-    Cv createCv(UUID userId, String title, String fileUrl);
-
+    Cv createCv(UUID userId, String title, MultipartFile file);
     List<Cv> getCvsByUserId(UUID userId);
-
     Optional<Cv> getCvById(UUID cvId);
-
     Cv setPrimaryCv(UUID userId, UUID cvId);
-
     void deleteCv(UUID userId, UUID cvId);
-
     Cv updateAiParsedContent(UUID cvId, String jsonContent);
 }
