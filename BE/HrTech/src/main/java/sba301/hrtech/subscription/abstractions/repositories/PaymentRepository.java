bd89546895fd57f8c2f03fbde2e1172c1034a1e7
myapah76 <140422521+myapah76@users.noreply.github.com>
2026-06-07 15:03:55 +0700

    Add: payment vnpay field

--- BE/HrTech/src/main/java/sba301/hrtech/subscription/abstractions/repositories/PaymentRepository.java
+++ BE/HrTech/src/main/java/sba301/hrtech/subscription/abstractions/repositories/PaymentRepository.java
@@ -3,9 +3,17 @@ package sba301.hrtech.subscription.abstractions.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sba301.hrtech.subscription.entities.Payment;
+import sba301.hrtech.subscription.entities.enums.PaymentMethod;
+import sba301.hrtech.subscription.entities.enums.PaymentStatus;
+
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, UUID> {
+    Optional<Payment> findByTxnRef(String txnRef);
+    List<Payment> findByStatus(PaymentStatus status);
+    List<Payment> findByPaymentMethod(PaymentMethod method);
 }
 
