09d941322239e90defb1154a320b95c75255877a
Trung <voquangtrungyb@gmail.com>
2026-06-29 11:35:23 +0700

    feat: implement ApplicationService logic and repository interface for job application workflows

--- BE/HrTech/src/main/java/sba301/hrtech/application/abstractions/repositories/ApplicationRepository.java
+++ BE/HrTech/src/main/java/sba301/hrtech/application/abstractions/repositories/ApplicationRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sba301.hrtech.application.entities.Application;
 
+import sba301.hrtech.application.entities.enums.ApplicationStatus;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -14,4 +16,5 @@ public interface ApplicationRepository extends JpaRepository<Application, UUID> 
     List<Application> findByJobId(UUID jobId);
     Optional<Application> findByInterviewResponseToken(String interviewResponseToken);
     boolean existsByUserIdAndJobId(UUID userId, UUID jobId);
+    boolean existsByUserIdAndJobIdAndStatusNotIn(UUID userId, UUID jobId, java.util.Collection<ApplicationStatus> statuses);
 }
